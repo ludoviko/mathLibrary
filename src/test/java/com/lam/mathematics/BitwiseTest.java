d2838d43af128229d44f1c786d9d1a79394539d4
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-10 01:13:53 +0200

    Bitwise test..

--- src/test/java/com/lam/mathematics/BitwiseTest.java
+++ src/test/java/com/lam/mathematics/BitwiseTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.hamcrest.CoreMatchers;
 
 import com.lam.mathematics.Bitwise;
 
@@ -24,4 +25,15 @@ public class BitwiseTest {
 		Assert.assertEquals(1, Bitwise.findBit(3, 1));
 		Assert.assertEquals(0, Bitwise.findBit(3, 2));
 	}
+
+	@Test
+	public void testCountBits() {
+        Assert.assertThat("Should be the same.", Bitwise.countBits(0), CoreMatchers.is(0));
+        Assert.assertThat("Should be the same.", Bitwise.countBits(1), CoreMatchers.is(1));
+        Assert.assertThat("Should be the same.", Bitwise.countBits(2), CoreMatchers.is(1));
+        Assert.assertThat("Should be the same.", Bitwise.countBits(3), CoreMatchers.is(2));
+        Assert.assertThat("Should be the same.", Bitwise.countBits(31), CoreMatchers.is(5));
+//        Assert.assertThat("Should be the same.", Bitwise.countBits(), CoreMatchers.is(1));
+	}
+
 }
