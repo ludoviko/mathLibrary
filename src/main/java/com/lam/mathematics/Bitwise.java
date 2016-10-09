dd1a5999ce92e096935fc74f1c8961c7c7d59e23
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-10 01:14:23 +0200

    Bitwise method.

--- src/main/java/com/lam/mathematics/Bitwise.java
+++ src/main/java/com/lam/mathematics/Bitwise.java
@@ -8,11 +8,25 @@ public class Bitwise {
 		return i;
 	}
 
+    public static  int  countBits(int n) {
+        // Position: Zero based, starting from right.
+        int bits = 0;
+
+        while (n  > 0) {
+            bits +=  n & 1;
+            n = n >> 1;
+        }
+
+        return bits;
+    }
+
     public static void main(String[] s) {
         System.out.println(findBit(13, 0));
         System.out.println(findBit(13, 1));
         System.out.println(findBit(13, 2));
         System.out.println(findBit(13, 3));
         System.out.println(findBit(13, 4));
+        System.out.println(countBits(13));
+        System.out.println(countBits(31));
     }
 }
