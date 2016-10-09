f38ea5115bbb89ad029c93834f8fdf5c7c6d6b0f
L.Azuaje <ludovikoazuaje@yahoo.com>
2016-10-10 01:02:39 +0200

    Bitwise doc.

--- src/main/java/com/lam/mathematics/Bitwise.java
+++ src/main/java/com/lam/mathematics/Bitwise.java
@@ -3,7 +3,7 @@ package com.lam.mathematics;
 public class Bitwise {
 
 	public static byte findBit(int n, int position) {
-		// Position: Zero based, starting from left.
+		// Position: Zero based, starting from right.
 		byte i =  (byte) ((n >> position) & 1);
 		return i;
 	}
