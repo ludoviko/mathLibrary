4575bdfde75e768ca968b8cce9049ddd109f9d06
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-07-10 22:22:14 +0200

    Division algorithm.

---
+++ src/main/java/com/lam/mathematics/Division.java
@@ -0,0 +1,51 @@
+package com.lam.mathematics;
+
+/**
+ * Created by Usuario on 17/04/2017.
+ *
+ *  THE DIVISION ALGORITHM: Let a be an integer and d a positive integer. Then there
+ *  are unique integers q and r, with 0 ≤ r < d, such that a = dq + r.
+ *
+ *  From Discrete Mathematics, by Kenneth Rosen.
+ *
+ */
+public class Division {
+    private long quotient;
+    private long remainder;
+    private long dividend;
+    private long divisor;
+
+    public Division(long dividend, long divisor) {
+        this.dividend = dividend;
+        this.divisor = divisor;
+    };
+
+    public void calculate() {
+        this.quotient = Math.floorDiv(dividend, divisor);
+        this.remainder = Math.floorMod(dividend, divisor);
+    }
+
+    public long getRemainder() {
+        return this.remainder;
+    }
+
+    public long getQuotient() {
+        return this.quotient;
+    }
+
+    @Override
+    public String toString() {
+        return "Division{" +
+                "quotient=" + quotient +
+                ", remainder=" + remainder +
+                ", dividend=" + dividend +
+                ", divisor=" + divisor +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Division division = new Division(-111, 11);
+        division.calculate();
+        System.out.println(division);
+    }
+ }
