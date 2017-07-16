af7dd4d3ae8d745d8cae41fe25981532eaa0b6d2
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-07-16 02:28:38 +0200

    Update factorial.

--- src/main/java/com/lam/mathematics/Factorial.java
+++ src/main/java/com/lam/mathematics/Factorial.java
@@ -3,19 +3,7 @@ package com.lam.mathematics;
 import java.math.BigInteger;
 
 
-// TODO apply memorization
 public class Factorial {
-	public static long find(long n) {
-		long factorial = 1;
-		if (n == 0 || n == 1) {
-			return 1;
-		}
-		for (long i = n; i >= 1; i--) {
-			factorial *= i;
-		}
-		return factorial;
-	}
-
 	public static BigInteger findBI(int n) {
 		BigInteger big = new BigInteger("1");
 		
@@ -33,8 +21,7 @@ public class Factorial {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		long n = Factorial.find(9);
-		System.out.println(n);
+		System.out.println(Factorial.findBI(100));
 	}
 
 }
