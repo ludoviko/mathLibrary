ed179d9bc55234f57ff8f3342f3cd3cf3937ee6b
L.Azuaje <ludovikoazuaje@yahoo.com>
2017-09-06 02:28:39 +0200

    ArithmeticProgression.

---
+++ src/main/java/com/lam/mathematics/ArithmeticProgression.java
@@ -0,0 +1,156 @@
+package com.lam.mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * 
+ * @author L.Azuaje.
+ *
+ * Missing test.
+ *
+ */
+
+public class ArithmeticProgression {
+    private int step;
+    private int a1;
+    private int n;
+
+    public ArithmeticProgression(int a1, int step, int n) {
+        this.a1 = a1;
+        this.step = step;
+        this.n = n;
+    }
+
+	public int findAn() {
+        return this.a1 + (n-1)* step;
+	}
+
+    public int sum() {
+        return (int)(((this.a1 + findAn()) * this.n)/ 2.0);
+    }
+
+	public static void main(String[] args) throws IOException {
+		MyScanner scanner = new MyScanner();
+		PrintStream out = System.out;
+
+		int n = scanner.nextInt();
+		int[] data;
+
+        ArithmeticProgression main;
+
+        for (int i = 0; i < n; i++) {
+            data = scanner.nextIntArray();
+            main = new ArithmeticProgression(data[0], data[1], data[2]);
+            out.format("%d ", main.sum());
+        }
+
+		out.println();
+
+		scanner.reader.close();
+		out.close();
+	}
+
+	// -----------MyScanner class for faster input----------
+	public static class MyScanner {
+		BufferedReader reader;
+
+		public MyScanner() {
+			this.reader = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public void close() throws IOException {
+			this.reader.close();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(this.next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(this.next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(this.next());
+		}
+
+		String next() {
+			String str = "";
+			try {
+				str = this.reader.readLine().trim();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		String[] nextStringArray() {
+			String[] str = null;
+			try {
+				str = this.reader.readLine().trim().split(" ");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray() {
+			String[] data  = nextStringArray();
+			int[] a  = new int[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		Integer[] nextIntegerArray() {
+			String[] data  = nextStringArray();
+			Integer[] a  = new Integer[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Integer.parseInt(data[i]);
+			}
+
+			return a;
+		}
+
+		long[] nextLongArray() {
+			String[] data  = nextStringArray();
+			long[] a  = new long[data.length];
+
+			for (int i = 0; i < a.length; i++) {
+				a[i] = Long.parseLong(data[i]);
+			}
+
+			return a;
+		}
+
+          float[] nextFloatArray() {
+            String[] data  = nextStringArray();
+            float[] a  = new float[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Float.parseFloat(data[i]);
+            }
+
+            return a;
+        }
+
+        double[] nextDoubleArray() {
+            String[] data  = nextStringArray();
+            double[] a  = new double[data.length];
+
+            for (int i = 0; i < a.length; i++) {
+                a[i] = Double.parseDouble(data[i]);
+            }
+
+            return a;
+        }
+    }
+}
+
