e553d1c6c65b271e7df4a8d8bc0dd7b9e5908662
L.Azuaje <ludovikoazuaje@yahoo.com>
2018-03-31 23:56:47 +0200

    Palidrome class.

---
+++ src/main/java/com/lam/mathematics/Palindrome.java
@@ -0,0 +1,35 @@
+package com.lam.mathematics;
+
+/**
+ * Palindrome for integers.
+ */
+public class Palindrome {
+
+    public static boolean isPalindrome(char[] array) {
+        boolean result = true;
+
+        for (int i = 0; i < array.length / 2 + 1; i++) {
+            if (array[i] == array[array.length - 1 - i]) {
+            } else {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean isPalindrome(Integer[] array) {
+        boolean result = true;
+
+        for (int i = 0; i < array.length / 2 + 1; i++) {
+            if (array[i] == array[array.length - 1 - i]) {
+            } else {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
