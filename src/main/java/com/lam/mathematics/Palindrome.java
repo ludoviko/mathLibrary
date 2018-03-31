16f50da2cf606d7adb09b8431bb991f1aaa731b9
L.Azuaje <ludovikoazuaje@yahoo.com>
2018-04-01 00:03:53 +0200

    Palindrome update.

--- src/main/java/com/lam/mathematics/Palindrome.java
+++ src/main/java/com/lam/mathematics/Palindrome.java
@@ -8,7 +8,7 @@ public class Palindrome {
     public static boolean isPalindrome(char[] array) {
         boolean result = true;
 
-        for (int i = 0; i < array.length / 2 + 1; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             if (array[i] == array[array.length - 1 - i]) {
             } else {
                 result = false;
@@ -22,7 +22,7 @@ public class Palindrome {
     public static boolean isPalindrome(Integer[] array) {
         boolean result = true;
 
-        for (int i = 0; i < array.length / 2 + 1; i++) {
+        for (int i = 0; i < array.length / 2; i++) {
             if (array[i] == array[array.length - 1 - i]) {
             } else {
                 result = false;
