7180af48f718424ae6de27b978322643788cad44
ludoviko <ludovikoazuaje@yahoo.com>
2019-08-19 21:27:23 +0200

    PascalTriangle.

--- src/main/java/com/lam/mathematics/PascalTriangle.java
+++ src/main/java/com/lam/mathematics/PascalTriangle.java
@@ -1,59 +1,28 @@
 package com.lam.mathematics;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.lam.annotation.Note;
 
 
 @Note(note="Testing missing")
-@Deprecated
 public class PascalTriangle {
-	
+
 	/**
-	 * Find a a single Pascal element from a given row.
-	 * 
+     * Find a a single Pascal element from a given row and column. For not existing
+     * rows and columns the method returns zero.
+     *
 	 * @param row: The row is zero based.
-	 * @param column
+     * @param column: The column is zero based.
 	 * @return the Pascal element
 	 */
-	public Long findPascalElement(int row, int column) {
-		Long element = Combinatorics.combinations(row, column);
-		return element;
-	}
-
-	/**
-	 * 
-	 * 	Find the Pascal elements from the Pascal Triangle that are in a given row. 
-	 * 
-	 * @param row: The row is zero based.
-	 * @return: an array with the row's elements.
-	 */
-	public long[] findPascalRow(int row) {
-		long[] array = new long[row + 1]; 
-		
-		for (int i = 0; i < array.length; i++) {
-			array[i] = findPascalElement(row, i);
+    public static long pascal(int row, int column) {
+        if (row == column) {
+            return 1;
+        } else if (column == 0) {
+            return 1;
+        } else if (row > 0 && column > 0) {
+            return pascal(row - 1, column - 1) + pascal(row - 1, column);
+        } else {
+            return 0;
 		}
-		return array;
 	}
-
-
-	/**
-	 * Find the Pascal elements from the Pascal Triangle. 
-	 * 
-	 * @param rows: The rows are zero based. 
-	 * @return: a list that contains the Palcal's elements.
-	 */
-	public List<Long> findPascal(int rows) {
-	 List<Long> list = new ArrayList<Long>();
-	 
-		for (int row = 0; row <= rows; row++) {
-			for (int i = 0; i <= row; i++) {
-				list.add(findPascalElement(row, i));
-			}
-		}
-		return list;
-	}
-	
 }
